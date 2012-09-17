5166121b37c751945bb466b288e9f34e2eaf23d7
Bastien Clément <g@ledric.me>
2012-09-17 22:17:53 +0200

    Fix a bug with the SoR button hiding

--- BNFriendsCounter.lua
+++ BNFriendsCounter.lua
@@ -1,4 +1,7 @@
 function BNFriendsCounter_OnEvent()
-	local bnetCount, bnetOnline = BNGetNumFriends()
-	BNFriendsCounterCounter:SetText(bnetCount.."|cff416380/100|r")
+	local bnetCount, bnetOnline = BNGetNumFriends();
+	BNFriendsCounterCounter:SetText(bnetCount.."|cff416380/100|r");
+
+	FriendsTabHeaderSoRButton:RegisterEvent("SOR_BY_TEXT_UPDATED");
+	FriendsTabHeaderSoRButton:Hide();
 end
