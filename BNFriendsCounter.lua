bb20c124e06273ce036d7b283c53cf319e5178fa
galedric <g@ledric.me>
2013-02-07 15:42:08 +0100

    You can only have 99 friends

--- BNFriendsCounter.lua
+++ BNFriendsCounter.lua
@@ -1,6 +1,6 @@
 function BNFriendsCounter_OnEvent()
 	local bnetCount, bnetOnline = BNGetNumFriends();
-	BNFriendsCounterCounter:SetText(bnetCount.."|cff416380/100|r");
+	BNFriendsCounterCounter:SetText(bnetCount.."|cff416380/99|r");
 
 	FriendsTabHeaderSoRButton:RegisterEvent("SOR_BY_TEXT_UPDATED");
 	FriendsTabHeaderSoRButton:Hide();
